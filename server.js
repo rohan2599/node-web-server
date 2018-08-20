3041328ff25e6635a8f14d1dd9ababea84d84d53
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-08-20 21:11:16 +0530

    updated package.json and server file

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const hbs = require('hbs');
 const fs =require('fs');
 
 var app = express();
+const port = process.env.PORT || 3000;
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-//app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>{
   var flag = new Date().toString();
   var log = `${flag}:${req.method} ${req.url}`;
@@ -20,11 +21,11 @@ app.use((req,res,next)=>{
 });
 
 // this middleware does not let execute the home page and other page
-app.use((req,res,next)=>{
-  res.render('maintenance.hbs');
- });
+//app.use((req,res,next)=>{
+//  res.render('maintenance.hbs');
+ //});
 
-app.use(express.static(__dirname+'/public'));
+//app.use(express.static(__dirname+'/public'));
 
 
 hbs.registerHelper('getCurrentYear',()=>{
@@ -59,6 +60,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`server is on the port ${port}`);
 });
